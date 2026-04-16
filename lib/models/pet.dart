f83674a72ded4832b8e75c7b5c4833e8ae0543a1
Liukeyu <liukele015@gmail.com>
2026-04-16 21:35:31 +0800

    添加宠物模型和类型枚举，定义宠物属性和进化链

---
+++ lib/models/pet.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+enum PetType {
+  fire(Color(0xFFBD4A20), "火系"),
+  water(Color.fromARGB(255, 40, 158, 255), "水系"),
+  grass(Color.fromARGB(255, 78, 188, 115), "草系"),
+  light(Color.fromARGB(255, 79, 193, 255), "光系"),
+  ordinary(Color.fromARGB(255, 97, 152, 177), "普通"),
+  dragon(Color.fromARGB(255, 228, 43, 43), "龙系"),
+  poison(Color.fromARGB(255, 163, 100, 207), "毒系"),
+  insect(Color.fromARGB(255, 151, 179, 70), "虫系"),
+  valiant(Color.fromARGB(255, 255, 129, 79), "武系"),
+  wing(Color.fromARGB(255, 71, 209, 219), "翼系"),
+  cute(Color.fromARGB(255, 255, 128, 147), "萌系"),
+  evil(Color.fromARGB(255, 233, 64, 120), "恶系"),
+  mechanical(Color.fromRGBO(62, 194, 161, 1), "机械系"),
+  magical(Color.fromARGB(255, 189, 164, 250), "幻系"),
+  electricity(Color.fromARGB(255, 240, 200, 80), "电系"),
+  dark(Color.fromARGB(255, 157, 86, 207), "幽系");
+
+  final Color themeColor;
+  final String label;
+  const PetType(this.themeColor, this.label);
+}
+
+class Pet {
+  final String name;
+  final String id;
+  final PetType type;
+  final List<double> stats;
+  final List<String> evolutions; 
+
+  Pet({required this.name, required this.id, required this.type, required this.stats, required this.evolutions});
+}
