647e5651eca4def8a9beaadfdcdccae5f6f36d5b
Liukeyu <liukele015@gmail.com>
2026-04-19 01:10:36 +0800

    新增 RocoPlugin 抽象类，定义插件接口和必要方法

---
+++ lib/models/plugin_interface.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+abstract class RocoPlugin {
+  String get id;
+  String get name;
+  String get description;
+  String get version;
+  String get author;
+
+  // 插件在列表中的图标
+  Widget buildIcon(BuildContext context, Color accentColor);
+
+  // 插件的主入口 UI
+  Widget buildEntryPage(BuildContext context, Color accentColor);
+}
