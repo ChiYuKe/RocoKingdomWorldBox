175015a38fa54b631d272f425e46ec554ce92102
Liukeyu <liukele015@gmail.com>
2026-04-19 01:10:41 +0800

    新增 CalcPlugin 类，实现精灵对比功能，包含插件接口和 UI 组件

---
+++ lib/plugins/calc_plugin/main.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import '../../models/plugin_interface.dart';
+import '../../models/pet.dart';
+import '../../widgets/plugin_page_template.dart'; // 引入模板
+import 'widgets/contrast_ui.dart';
+
+class CalcPlugin implements RocoPlugin {
+  final List<Pet> pokedex;
+  CalcPlugin({required this.pokedex});
+
+  @override
+  String get id => "com.roco.plugin.calc";
+  @override
+  String get name => "精灵对比";
+  @override
+  String get description => "对比两只精灵的属性，帮助你更好地了解它们的优劣势";
+  @override
+  String get version => "V 2.1.0";
+  @override
+  String get author => "RocoCoder";
+
+  @override
+  Widget buildIcon(BuildContext context, Color accentColor) {
+    return Icon(Icons.analytics_rounded, color: accentColor);
+  }
+
+  @override
+  Widget buildEntryPage(BuildContext context, Color accentColor) {
+    // 使用通用模板包装私有 UI
+    return PluginPageTemplate(
+      title: name,
+      subTitle: description, 
+      accentColor: accentColor,
+      body: ContrastUI(
+        accentColor: accentColor,
+        pokedex: pokedex,
+      ),
+    );
+  }
+}
